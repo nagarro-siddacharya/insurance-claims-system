3b5df2ab4c185368d4173540dc225654fed4e164
Siddhant Acharya <Siddhant.Acharya@Amway.com>
2026-07-25 16:28:05 +0530

    downgraded prisma version

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Connected to Postgresql');
+  }
+  async onModuleDestroy() {
+    await this.$connect();
+    console.log('DB disconnected');
+  }
+}
